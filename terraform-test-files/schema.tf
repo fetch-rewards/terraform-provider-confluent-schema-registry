c5b025d762f0e63aec9739bdec30fe295a203f33
Patrick Maziarz <p.maziarz@fetchrewards.com>
2024-08-23 11:04:18 -0500

    test: modified test files to have an additional proto topic and schema to test locally against

--- terraform-test-files/schema.tf
+++ terraform-test-files/schema.tf
@@ -14,3 +14,20 @@ resource "schemaregistry_schema" "test_provider_topic_schema" {
   schema_type = "AVRO"
   subject = "provider-test-schema"
 }
+
+resource "kafka_topic" "test_provider_topic_proto" {
+  name               = "provider-test-topic-proto"
+  partitions         = 1
+  replication_factor = 1
+  config = {
+    "min.insync.replicas": "1"
+  }
+  provider= kafka.event-tracking-dev
+}
+
+resource "schemaregistry_schema" "test_provider_topic_schema_proto" {
+  provider= schemaregistry.schema-registry-dev
+  schema = "syntax = \"proto3\";package com.fetchrewards.locationservice.proto;\n\n\noption java_outer_classname = \"FidoLocationTrackerProto\";\n\nmessage FidoLocationTracker {\n  string location_id = 1;\n  string fido = 2;}"
+  schema_type = "PROTOBUF"
+  subject = "provider-test-schema-proto"
+}

6a54a0397cc41db8b11b1567ca8c880970823414
Patrick Maziarz <p.maziarz@fetchrewards.com>
2024-08-27 09:24:32 -0500

    feat: updated test-new-build.sh with working commands

--- test-new-build.sh
+++ test-new-build.sh
@@ -1,5 +1,3 @@
-# make build
-# cp dist/terraform-provider-schemaregistry ~/.terraform.d/plugins/local/fetch-rewards/confluent-schema-registry/1.1.0/linux_amd64/terraform-provider-confluent-schema-registry_1.1.0
 make local_install
 cd terraform-test-files || exit
 rm -rf .terraform/providers
